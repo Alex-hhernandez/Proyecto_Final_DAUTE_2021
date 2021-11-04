77c6a990fd549abdcd20f463d0aa998d7073e2c6
Alex-hhernandez <82846948+Alex-hhernandez@users.noreply.github.com>
2021-11-04 10:19:07 -0600

    Se agrego la funcionalidad de listar

--- app/src/main/java/com/acdi/proyecto_final_daute/DtoUser.java
+++ app/src/main/java/com/acdi/proyecto_final_daute/DtoUser.java
@@ -2,16 +2,101 @@ package com.acdi.proyecto_final_daute;
 
 public class DtoUser {
 
-    private int id;
+    private String id;
     private String nombre;
     private String apellidos;
     private String correo;
     private String usuario;
     private String clave;
-    private int tipo;
-    private int estado;
+    private String tipo;
+    private String estado;
     private String pregunta;
     private String respuesta;
 
-    
+
+    public DtoUser() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    @Override
+    public String toString(){return nombre;}
 }
--- app/src/main/java/com/acdi/proyecto_final_daute/List_User.java
+++ app/src/main/java/com/acdi/proyecto_final_daute/List_User.java
@@ -1,14 +1,73 @@
 package com.acdi.proyecto_final_daute;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class List_User extends AppCompatActivity {
+
+    ListView listaUsers;
+    private List<DtoUser> lista = new ArrayList<DtoUser>();
+    ArrayAdapter<DtoUser> adaptador;
+    DatabaseReference databaseReference;
+    DatabaseReference databaseReference1;
+    DtoUser datos = new DtoUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_user);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference1 = FirebaseDatabase.getInstance().getReference(getString(R.string.Usuario));
+
+        listaUsers = findViewById(R.id.ListaUser);
+
+        listarDatos();
+    }
+
+    private void listarDatos(){
+
+        databaseReference.child("Usuario").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //lista.clear();
+
+                for(DataSnapshot snapshot1 : snapshot.getChildren()){
+
+                    DtoUser user = snapshot1.getValue(DtoUser.class);
+
+                    lista.add(user);
+                    adaptador = new ArrayAdapter<DtoUser>(List_User.this, android.R.layout.simple_list_item_1, lista);
+                    listaUsers.setAdapter(adaptador);
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(List_User.this, "Ha ocurrido un error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
