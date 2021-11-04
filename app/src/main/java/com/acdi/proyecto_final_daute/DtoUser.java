ed1d696658154373fae80945dd4f212c675738ed
Alex-hhernandez <82846948+Alex-hhernandez@users.noreply.github.com>
2021-11-04 09:13:20 -0600

    Dto

---
+++ app/src/main/java/com/acdi/proyecto_final_daute/DtoUser.java
@@ -0,0 +1,17 @@
+package com.acdi.proyecto_final_daute;
+
+public class DtoUser {
+
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String usuario;
+    private String clave;
+    private int tipo;
+    private int estado;
+    private String pregunta;
+    private String respuesta;
+
+    
+}
