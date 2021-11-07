95c20b0eef2aa993c8de7c21524566ade8318122
carmen30-Isa <82846294+carmen30-Isa@users.noreply.github.com>
2021-11-07 15:48:04 -0600

    Vista para editar categorias

---
+++ app/src/main/java/com/acdi/proyecto_final_daute/Edit_Category.java
@@ -0,0 +1,14 @@
+package com.acdi.proyecto_final_daute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_category);
+    }
+}
