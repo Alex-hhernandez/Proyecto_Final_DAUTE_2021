8db8e84c96a17100216ff437c5b2588f6ea8d59d
David-FV <floresdavide0@gmail.com>
2021-11-09 09:42:22 -0600

    Se agrego la funcionalidad para iniciar sesión

--- app/src/main/java/com/acdi/proyecto_final_daute/Login.java
+++ app/src/main/java/com/acdi/proyecto_final_daute/Login.java
@@ -1,8 +1,10 @@
 package com.acdi.proyecto_final_daute;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -14,34 +16,60 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
 
     private Window window;
+    private EditText et_email, et_pass;
+    private Button btnLogin;
+    DatabaseReference databaseReference1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        et_email = findViewById(R.id.et_email);
+        et_pass = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_getstarted);
+        databaseReference1 = FirebaseDatabase.getInstance().getReference(getString(R.string.Usuario));
+
         this.window = getWindow();
 
-        CustomLogin();
-        IniciarSeccion();
+        //IniciarSeccion();
+
         cambiarcolor();
+        CustomLogin();
 
-    }
-
-    private void IniciarSeccion(){
-        Button btnsuccess = findViewById(R.id.btn_getstarted);
-
-        btnsuccess.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                welcome();
+
+                String email = et_email.getText().toString();
+                String pass = et_pass.getText().toString();
+
+                if (email.length() == 0){
+                    et_email.setError("Campo obligatorio");
+
+                }else if(pass.length() == 0){
+                    et_pass.setError("Campo obligatorio");
+
+                }else{
+                    login(getApplicationContext(), email, pass);
+                }
             }
         });
+
     }
 
     private void CustomLogin(){
@@ -83,9 +111,44 @@ public class Login extends AppCompatActivity {
                 Intent home = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(home);
                 dialog.dismiss();
+                finish();
             }
         },1500);
 
         dialog.show();
+    }
+
+    private void login(final Context context, final String email, final String pass){
+
+        Query query = databaseReference1.orderByChild("correo").equalTo(email);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.exists()){
+                    for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+
+                        DtoUser user = dataSnapshot.getValue(DtoUser.class);
+
+                        String clave = user.getClave();
+
+                        if (pass.equals(clave)){
+                            welcome();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "Usuario no encontrado", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
